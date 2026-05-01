d1edf8d3d82879e90762fe74925ccaeda9170e42
SHRI DAYAL VERMA <shridayalverma896@gmail.com>
2026-05-01 12:56:09 +0530

    updated files

--- day-4/server.js
+++ day-4/server.js
@@ -14,10 +14,14 @@ app.get("/notes",(req,res) => {
 
 app.delete("/notes/:index",(req,res) => {
     delete notes[req.params.index];
-    console.log(req.params);
+    console.log(req.params);    
     res.send("Note deleted successfully");
 });
 
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description;
+    res.send("Update Successfully");
+})
 
 
 app.listen(3000,()=>{
